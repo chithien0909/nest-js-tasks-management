fe644ff68fc48e7cc09715644bc81ffbeaa55004
chithien0909 <chithien0909@gmail.com>
2020-01-04 15:41:37 +0700

    first commit

---
+++ src/tasks/tasks.model.ts
@@ -0,0 +1,14 @@
+import { TasksService } from './tasks.service';
+import { TasksModule } from './tasks.module';
+export interface Task{
+  id: string;
+  title: string;
+  description: string;
+  status: TasksStatus;
+}
+
+export enum TasksStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
